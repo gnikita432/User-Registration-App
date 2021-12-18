3421bd345f5f03eaf264d67b69fdac85338c7d9f
Nikita Gupta <gnikita432@gmail.com>
2021-12-18 12:11:28 +0530

    login, register frontend- state

--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./login.css";
 
 const Login = () => {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
+
   return (
     <div className="login">
       <h1>Login</h1>
-      <input type="text" name="email" placeholder="Enter your Email"></input>
+      <input
+        type="text"
+        name="email"
+        value={user.email}
+        onChange={handleChange}
+        placeholder="Enter your Email"
+      ></input>
       <input
         type="password"
         name="password"
+        value={user.password}
+        onChange={handleChange}
         placeholder="Enter your Password"
       ></input>
       <div className="button">Login</div>
