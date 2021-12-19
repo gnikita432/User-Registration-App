0bc2cf7fe343a7dbdbccff98afe934218bdad070
Nikita Gupta <gnikita432@gmail.com>
2021-12-19 19:55:49 +0530

    added routing

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,30 @@ import "./App.css";
 import Login from "./components/login/login";
 import Register from "./components/register/register";
 import Profile from "./components/profile/profie";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
+  const [user, setLoginUser] = useState({});
   return (
     <div className="App">
-      <Login />
-      {/* <Register /> */}
-      {/* <Profile /> */}
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            {user && user._id ? (
+              <Profile setLoginUser={setLoginUser} />
+            ) : (
+              <Login setLoginUser={setLoginUser} />
+            )}
+          </Route>
+          <Route path="/login">
+            <Login setLoginUser={setLoginUser} />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
